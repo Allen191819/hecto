e747a2b36f8b75b5c3749e91e3f7589f44be5726
Allen191819 <2020302191891@whu.edu.cn>
2022-11-20 19:38:14 +0800

    Chapter 6

--- src/row.rs
+++ src/row.rs
@@ -1,3 +1,4 @@
+use crate::SearchDirection;
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 #[derive(Default)]
@@ -101,5 +102,42 @@ impl Row {
     }
     pub fn as_bytes(&self) -> &[u8] {
         self.string.as_bytes()
+    }
+    pub fn find(&self, query: &str, at: usize, direction: SearchDirection) -> Option<usize> {
+        if at > self.len {
+            return None;
+        }
+        let start = if direction == SearchDirection::Forward {
+            at
+        } else {
+            0
+        };
+        let end = if direction == SearchDirection::Forward {
+            self.len
+        } else {
+            at
+        };
+        let substring: String = self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+            .collect();
+
+        let matching_bytes_index = if direction == SearchDirection::Forward {
+            substring.find(query)
+        } else {
+            substring.rfind(query)
+        };
+        if let Some(matching_bytes_index) = matching_bytes_index {
+            for (grapheme_index, (byte_index, _)) in
+                substring[..].grapheme_indices(true).enumerate()
+            {
+                if matching_bytes_index == byte_index {
+                    #[allow(clippy::integer_arithmetic)]
+                    return Some(start + grapheme_index);
+                }
+            }
+        }
+        None
     }
 }
